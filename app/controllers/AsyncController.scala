87ef93ee04cc897955a27cb064f60a6984bd42d2
Remko de Jong <aggenebbisj@gmail.com>
2016-08-21 23:14:25 +0200

    Added PostgreSQL integration with Slick

--- app/controllers/AsyncController.scala
+++ app/controllers/AsyncController.scala
@@ -2,9 +2,13 @@ package controllers
 
 import akka.actor.ActorSystem
 import javax.inject._
+
 import play.api._
+import play.api.db.slick.DatabaseConfigProvider
 import play.api.mvc._
-import scala.concurrent.{ExecutionContext, Future, Promise}
+import slick.driver.JdbcProfile
+
+import scala.concurrent.{Await, ExecutionContext, Future, Promise}
 import scala.concurrent.duration._
 
 /**
@@ -18,24 +22,31 @@ import scala.concurrent.duration._
  * asynchronous code.
  */
 @Singleton
-class AsyncController @Inject() (actorSystem: ActorSystem)(implicit exec: ExecutionContext) extends Controller {
+class AsyncController @Inject() (dbConfigProvider: DatabaseConfigProvider, actorSystem: ActorSystem)(implicit exec: ExecutionContext) extends Controller {
 
-  /**
-   * Create an Action that returns a plain text message after a delay
-   * of 1 second.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/message`.
-   */
+  val dbConfig = dbConfigProvider.get[JdbcProfile]
+  val db = dbConfig.db
+  import dbConfig.driver.api._
+  private val Msgs = TableQuery[MsgTable]
+
   def message = Action.async {
-    getFutureMessage(1.second).map { msg => Ok(msg) }
+    getFutureMessage.map(msg => msg.map(msg => Ok(msg.msg)).getOrElse(NotFound))
   }
 
-  private def getFutureMessage(delayTime: FiniteDuration): Future[String] = {
-    val promise: Promise[String] = Promise[String]()
-    actorSystem.scheduler.scheduleOnce(delayTime) { promise.success("Hi!") }
-    promise.future
+  private def _findById(id: Long): DBIO[Option[Msg]] = Msgs.filter(_.id === id).result.headOption
+
+  private def getFutureMessage: Future[Option[Msg]] = db.run(_findById(1))
+
+  private class MsgTable(tag: Tag) extends Table[Msg](tag, "msg") {
+
+    def id = column[Long]("id", O.AutoInc, O.PrimaryKey)
+    def msg = column[String]("msg")
+
+    def * = (id, msg) <> (Msg.tupled, Msg.unapply)
+    def ? = (id.?, msg.?).shaped.<>({ r => import r._; _1.map(_ => Msg.tupled((_1.get, _2.get))) }, (_: Any) => throw new Exception("Inserting into ? projection not supported."))
+
   }
+
+  case class Msg(id: Long, msg: String)
 
 }
--- build.sbt
+++ build.sbt
@@ -15,7 +15,8 @@ libraryDependencies ++= Seq(
   ws,
   "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test,
   "com.typesafe.play" %% "play-slick" % "2.0.2",
-  "com.typesafe.play" %% "play-slick-evolutions" % "2.0.2"
+  "org.postgresql" % "postgresql" % "9.4-1201-jdbc4"
+//  "com.typesafe.play" %% "play-slick-evolutions" % "2.0.2"
 )
 
 val gitHeadCommitSha = settingKey[String]("Determines the current git commit SHA")
